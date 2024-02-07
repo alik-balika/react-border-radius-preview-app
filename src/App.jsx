923a06ea1c9971825199c9756138f808ce4eb9f4
alik-balika <balikaalik@gmail.com>
2024-02-07 14:46:20 -0800

    enable user to change inputs and have the corresponding box change its border-radius

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,35 @@
-import React from "react";
+// eslint-disable-next-line no-unused-vars
+import React, { useState } from "react";
+import BorderRadiusInput from "./BorderRadiusInput";
 
 const App = () => {
-  return <div>App</div>;
+  const [topLeft, setTopLeft] = useState(0);
+  const [topRight, setTopRight] = useState(0);
+  const [bottomLeft, setBottomLeft] = useState(0);
+  const [bottomRight, setBottomRight] = useState(0);
+
+  const boxStyle = {
+    width: "300px",
+    height: "300px",
+    border: "1px solid black",
+    backgroundColor: "red",
+    borderRadius: `${topLeft}px ${topRight}px ${bottomRight}px ${bottomLeft}px`,
+  };
+
+  return (
+    <div style={{ color: "white" }}>
+      <h1>Border Radius Previewer</h1>
+      <div className="inputs">
+        <BorderRadiusInput label={"Top Left: "} setter={setTopLeft} />
+        <BorderRadiusInput label={"Top Right: "} setter={setTopRight} />
+        <BorderRadiusInput label={"Bottom Left: "} setter={setBottomLeft} />
+        <BorderRadiusInput label={"Bottom Right: "} setter={setBottomRight} />
+      </div>
+      <div className="center">
+        <div style={boxStyle} />
+      </div>
+    </div>
+  );
 };
 
 export default App;
---
+++ src/BorderRadiusInput.jsx
@@ -0,0 +1,26 @@
+// eslint-disable-next-line no-unused-vars
+import React from "react";
+import PropTypes from "react";
+
+const BorderRadiusInput = ({ label, setter }) => {
+  return (
+    <div>
+      <label>{label}</label>
+      <input
+        type="number"
+        defaultValue={0}
+        min={0}
+        max={300}
+        onChange={(e) => setter(e.target.value)}
+        className="styled-input"
+      />
+    </div>
+  );
+};
+
+BorderRadiusInput.propTypes = {
+  label: PropTypes.string.isRequired,
+  setter: PropTypes.func.isRequired,
+};
+
+export default BorderRadiusInput;
